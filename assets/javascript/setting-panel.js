881e0f3db9d7dcfc2f70298e755b1b3348c2425d
unknown <Maheshmagrati>
2025-12-04 15:02:48 +0545

    initial commit

---
+++ assets/javascript/setting-panel.js
@@ -0,0 +1,41 @@
+// // settings-panel.js
+
+// function SettingsPanel() {
+//   const settingsHTML = `
+//     <div class="setting-main">
+
+//     </div>
+
+//     <!-- Offcanvas Details -->
+//     <div class="offcanvas offcanvas-end" id="offcanvasRight2">
+//         <div class="offcanvas-header">
+//             <p class="offcanvas-title"><img src="assets/images/svg/logo.svg" alt="logo"></p>
+//             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+//         </div>
+//         <div class="offcanvas-body">
+//             <a href="https://1.envato.market/all-in-one-handyman-repair-services-html-template-home-rakshak"
+//                 class="demo-box" target="_blank">
+//                 <img class="w-100" src="assets/images/setting-demo/Handyman.jpg" alt="Handyman">
+//                 <p>Handyman Solution</p>
+//             </a>
+//             <a href="https://1.envato.market/all-in-one-plumbing-repair-services-html-template-home-rakshak"
+//                 class="demo-box" target="_blank">
+//                 <img class="w-100" src="assets/images/setting-demo/Plumbing.jpg" alt="Plumbing">
+//                 <p>Plumbing Solution</p>
+//             </a>
+//             <a href="https://1.envato.market/home-rakshak-roofing-services" class="demo-box" target="_blank">
+//                 <img class="w-100" src="assets/images/setting-demo/Roofing.jpg" alt="Roofing">
+//                 <p>Roofing Solution</p>
+//             </a>
+//             <a href="https://1.envato.market/home-rakshak-cleaning-services-html" class="demo-box" target="_blank">
+//                 <img class="w-100" src="assets/images/setting-demo/Cleaning.jpg" alt="Cleaning">
+//                 <p>Cleaning Solution</p>
+//             </a>
+//         </div>
+//     </div>
+//     `;
+
+//   const wrapper = document.createElement("div");
+//   wrapper.innerHTML = settingsHTML;
+//   document.body.appendChild(wrapper);
+// }
